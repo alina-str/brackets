f9e0d8d03bb62b58467c4a527d9c185d9e106212
alina-str <al-strukova@mail.ru>
2021-03-14 13:31:19 +0300

    feat: fix

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+        let str2 = str;
+        let halfLength = Math.floor(str2.length / 2);
+        for (let i = 0; i < halfLength; i++) {
+            for (let j = 0; j < bracketsConfig.length; j++) {
+                str2 = str2.replace(bracketsConfig[j].join(''), '');
+            }
+        }
+        return str2.length == 0;
+    }
 }
